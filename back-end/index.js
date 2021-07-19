3d6a953227e349a3b8bd74e89060fd70d939906a
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-07-19 15:13:19 +0530

    first commit

---
+++ back-end/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParses = require('body-parser')
+
+const app = express()
+const Port = 4500
+
+app.use(bodyParses.urlencoded({extended:true}))
+
+app.get('/', (req, res) =>{
+    console.log("Back-end is on!")
+    res.json({message: "We are good to go"})
+})
+
+app.listen(Port, console.log(`Running on PORT: ${Port}`))

813271320ee77580864abff2ef7b548617893321
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-07-20 13:17:18 +0530

    Back-end tweek

--- back-end/models/items.schema.js
+++ back-end/models/items.schema.js
@@ -3,9 +3,8 @@ const mongoose  = require("mongoose")
 var itemSchema = new mongoose.Schema({
     title: String,
     priority: String,
-    created: String,
     due: String
-})
+}, { timestamps: true })
 
 const Item = mongoose.model('Item', itemSchema)
 module.exports = {Item}
